c33a039a722b2c91a7d1a1706b1a4e8e8dc938cb
J-C HERON <myaddressmail@gmail.com>
2023-10-02 00:39:53 +0200

    fix: WebSecurityConfig pb

--- src/main/java/fr/caensup/sio/todo/WebSecurityConfiguration.java
+++ src/main/java/fr/caensup/sio/todo/WebSecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,16 +20,12 @@ import fr.caensup.sio.todo.services.DbUserService;
 public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests()
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((req)->req
         .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**"),
-        		AntPathRequestMatcher.antMatcher("/createUser/**"),
-        		AntPathRequestMatcher.antMatcher("/login/**")
+        		AntPathRequestMatcher.antMatcher("/createUser/**")
         		).permitAll()
             .anyRequest().authenticated()
-            .and()
-            .formLogin().loginPage("/login")
-            .and()
-            .headers().frameOptions().sameOrigin();
+        ).formLogin((form)-> form.loginPage("/login").permitAll()).headers((headers)->headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
         return http.build();
     }
     
